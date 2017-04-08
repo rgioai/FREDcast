4642b591ffcb9397ec743b270089840b2ddfd4ef
keithc <keithaf98@gmail.com>
2017-04-08 14:47:52 -0500

    NON-WORKING only for review purposes

--- norm_functions.py
+++ norm_functions.py
@@ -1,18 +1,10 @@
-import numpy as np
 import h5py
+import numpy as np
+
 
 def zero_one(data_column):
-    # TODO map to every value: ((x - x_min)/(x_max - x_min))
-    """
-    In pseudocode:
-    max = data_column.max
-    min = data_column.min
-    def norm(x):
-        return (x - min)/(max - min)
-    vfunc = np.vectorize(norm)
-    return vfunc(data_column)
-    """
-    # TODO return the new array
+    # ((x - x_min)/(x_max - x_min))
+
     max = np.amax(data_column)
     min = np.amin(data_column)
 
@@ -24,10 +16,10 @@ def zero_one(data_column):
 
 
 def percent_change(data_column):
-    # TODO map to every value (x[i]): ((x[i] - x[i-1])/x[i-1]); if i == 0: x[i] = 0
-    # TODO return the new array
+    # (x[i]): ((x[i] - x[i-1])/x[i-1]); if i == 0: x[i] = 0
 
-    n = data_column
+    n = np.empty_like(data_column)
+    n[:] = data_column
     for i in range(0, len(data_column)):
         if i == 0:
             data_column[i] = 0
@@ -37,8 +29,8 @@ def percent_change(data_column):
 
 
 def normal_dist(data_column):
-    # TODO map to every value: (x - x_mean)/x_std
-    # TODO return the new array
+    # (x - x_mean)/x_std
+
     mean = np.mean(data_column)
     std = np.std(data_column)
 
@@ -50,11 +42,10 @@ def normal_dist(data_column):
 
 
 def linear_residual(data_column, second_norm=None):
-    # TODO Calculate line from first to last for column, generate inference_function
-    # TODO Map to every value: x - inference_function(x)
-    # TODO Map second_norm to every value
-    # TODO return the new array
-    x = np.arange(1, len(data_column))
+    # line from first to last for column, generate inference_function
+    # x - inference_function(x)
+
+    x = np.arange(1, len(data_column) + 1).astype(np.float32)
     y = data_column
 
     line = np.polyfit(x, y, 1)
@@ -66,21 +57,19 @@ def linear_residual(data_column, second_norm=None):
     first_norm = vfunc(data_column)
 
     if second_norm is not None:
-        return second_norm(first_norm)
+        return second_norm(first_norm).astype(np.float32)
     else:
-        return first_norm
+        return first_norm.astype(np.float32)
 
 
 def exp_residual(data_column, date_column, second_norm=None):
-    # TODO Calculate CAGR for column, generate inference_function
-    # TODO Map to every value: x - inference_function(x)
-    # TODO Map second_norm to every value
-    # TODO return the new array
+    # CAGR for column, generate inference_function
+    # x - inference_function(x)
 
-    year_dif = int(str(date_column[-1])[2:6]) - int(str(date_column[0])[2:6])
+    year_dif = float(str(date_column[-1])[2:6]) - int(str(date_column[0])[2:6])
 
     def cagr_func(f, l, n):
-        return ((l / f) ^ (1 / n)) - 1
+        return ((l / f) ** (1 / n)) - 1
 
     cagr = cagr_func(data_column[0], data_column[-1], year_dif)
 
@@ -91,19 +80,18 @@ def exp_residual(data_column, date_column, second_norm=None):
     first_norm = vfunc(data_column)
 
     if second_norm is not None:
-        return second_norm(first_norm)
+        return second_norm(first_norm).astype(np.float32)
     else:
-        return first_norm
+        return first_norm.astype(np.float32)
 
 
 def gdp_residual(data_column, second_norm=None):
-    # TODO map to every value (x[i]): ((x[i] - x[i-1])/x[i-1]); if i == 0: x[i] = 0
-    # TODO map to every value x[i] - ((gdp[i] - gdp[i-1])/gdp[i-1]); if i == 0: gdp[i] = 0
-    # TODO map second_norm to every value
-    # TODO return the new array
+    # (x[i]): ((x[i] - x[i-1])/x[i-1]); if i == 0: x[i] = 0
+    # x[i] - ((gdp[i] - gdp[i-1])/gdp[i-1]); if i == 0: gdp[i] = 0
+
     n = percent_change(data_column)
 
-    #hdf5 file with gdp data
+    # hdf5 file with gdp data
     f = h5py.File('y_values.h5', 'r')
     gdp = np.asarray(f['gdp'])
 
@@ -111,9 +99,12 @@ def gdp_residual(data_column, second_norm=None):
         if i == 0:
             data_column[i] = 0
         else:
-            data_column[i] = ((n[i] - n[i - 1]) / n[i - 1])
-    return data_column
+            data_column[i] = (n[i] - ((gdp[i] - gdp[i - 1]) / gdp[i - 1]))
 
+    if second_norm is not None:
+        return second_norm(data_column).astype(np.float32)
+    else:
+        return data_column.astype(np.float32)
 
 def normalize_dataset(dataset, norm_fn, second_norm=None):
     """
@@ -122,9 +113,155 @@ def normalize_dataset(dataset, norm_fn, second_norm=None):
     Then use np.apply_along_axis to map norm_fn across the vertical axis of the dataset.
     Return the new array
     """
-    pass
+    '''
+    if norm_fn is not None:
+        if norm_fn is 'linear_residual':
+            return np.apply_along_axis(lambda x: linear_residual(x, second_norm))
+        elif norm_fn is 'exp_residual':
+            return np.apply_along_axis(lambda x: exp_residual(x, second_norm))
+        elif norm_fn is 'gdp_residual':
+            return np.apply_along_axis(lambda x: gdp_residual(x, second_norm))
+    return dataset'''
 
 
 if __name__ == '__main__':
     # TODO Unit testing for all functions
-    pass
+    import unittest
+
+
+    class UnitTester(unittest.TestCase):
+        def setUp(self):
+            pass
+
+        def test_zero_one(self):
+            test_data_column = np.array([1, 2, 3, 4, 5], dtype=np.float32)
+            solution = np.array([0, 0.25, 0.5, 0.75, 1], dtype=np.float32)
+            test_result = zero_one(test_data_column)
+
+            self.assertEqual(test_result.shape, (5,))
+            self.assertEqual(test_result.dtype, np.float32)
+            np.testing.assert_array_equal(test_result, solution)
+
+        def test_percent_change(self):
+            test_data_column = np.array([1, 2, 3, 4, 5], dtype=np.float32)
+            solution = np.array([0, 1, 0.5, 0.33333334, 0.25], dtype=np.float32)
+            test_result = percent_change(test_data_column)
+
+            self.assertEqual(test_result.shape, (5,))
+            self.assertEqual(test_result.dtype, np.float32)
+            np.testing.assert_array_equal(test_result, solution)
+
+        def test_normal_dist(self):
+            test_data_column = np.array([1, 2, 3, 4, 5], dtype=np.float32)
+            solution = np.array([-1.414214, -0.707107, 0, 0.707107, 1.414214], dtype=np.float32)
+            test_result = normal_dist(test_data_column)
+
+            self.assertEqual(test_result.shape, (5,))
+            self.assertEqual(test_result.dtype, np.float32)
+            np.testing.assert_array_almost_equal_nulp(test_result, solution, nulp=4)
+
+        def test_linear_residual(self):
+            test_data_column = np.array([1, 5, 7, 8, 11], dtype=np.float32)
+            solution = np.array([-0.8, -6, -8.6, -9.9, -13.8], dtype=np.float32)
+            test_result = linear_residual(test_data_column, second_norm=None)
+
+            self.assertEqual(test_result.shape, (5,))
+            self.assertEqual(test_result.dtype, np.float32)
+            np.testing.assert_array_equal(test_result, solution)
+
+            solution = np.array([1, 0.6, 0.4, 0.3, 0], dtype=np.float32)
+            test_result = linear_residual(test_data_column, second_norm=zero_one)
+
+            self.assertEqual(test_result.shape, (5,))
+            self.assertEqual(test_result.dtype, np.float32)
+            np.testing.assert_array_equal(test_result, solution)
+
+            solution = np.array([0, 6.5, 0.433333, 0.151163, 0.393939], dtype=np.float32)
+            test_result = linear_residual(test_data_column, second_norm=percent_change)
+
+            self.assertEqual(test_result.shape, (5,))
+            self.assertEqual(test_result.dtype, np.float32)
+            np.testing.assert_array_almost_equal_nulp(test_result, solution, nulp=14)
+
+            solution = np.array([1.625209, 0.42135, -0.180579, -0.481543, -1.38447], dtype=np.float32)
+            test_result = linear_residual(test_data_column, second_norm=normal_dist)
+
+            self.assertEqual(test_result.shape, (5,))
+            self.assertEqual(test_result.dtype, np.float32)
+            np.testing.assert_array_almost_equal_nulp(test_result, solution, nulp=274)
+
+        def test_exp_residual(self):
+            test_data_column = np.array([1, 5, 7, 8, 11], dtype=np.float32)
+            test_date_column = np.array(
+                ['1967-04-30', '1967-07-31', '1967-10-31', '1968-01-31', '1969-04-30'], dtype='S10')
+
+            #dates_file = h5py.File('y_dates.h5', 'r')
+            #dates = dates_file['gdp']
+            #print(np.asarray(dates))
+
+            solution = np.array([-1.316625, 2.683375, 4.683375, 5.683375, 8.683375], dtype=np.float32)
+            test_result = exp_residual(test_data_column, test_date_column, second_norm=None)
+
+            self.assertEqual(test_result.shape, (5,))
+            self.assertEqual(test_result.dtype, np.float32)
+            np.testing.assert_array_almost_equal_nulp(test_result, solution, nulp=2)
+
+            solution = np.array([0, 0.4, 0.6, 0.7, 1], dtype=np.float32)
+            test_result = exp_residual(test_data_column, test_date_column, second_norm=zero_one)
+
+            self.assertEqual(test_result.shape, (5,))
+            self.assertEqual(test_result.dtype, np.float32)
+            np.testing.assert_array_equal(test_result, solution)
+
+            solution = np.array([0, -3.038071, 0.74533, 0.213521, 0.527855], dtype=np.float32)
+            test_result = exp_residual(test_data_column, test_date_column, second_norm=percent_change)
+
+            self.assertEqual(test_result.shape, (5,))
+            self.assertEqual(test_result.dtype, np.float32)
+            np.testing.assert_array_almost_equal_nulp(test_result, solution, nulp=15)
+
+            solution = np.array([-1.625209, -0.42135, 0.180579, 0.481543, 1.38447], dtype=np.float32)
+            test_result = exp_residual(test_data_column, test_date_column, second_norm=normal_dist)
+
+            self.assertEqual(test_result.shape, (5,))
+            self.assertEqual(test_result.dtype, np.float32)
+            np.testing.assert_array_almost_equal_nulp(test_result, solution, nulp=274)
+
+        def test_gdp_residual(self):
+            test_data_column = np.array([1000, 2000, 4000, 3500, 4500], dtype=np.float32)
+            solution = np.array([-849450, -1698450, -3396450, -2971950, -3820950], dtype=np.float32)
+            test_result = linear_residual(test_data_column, second_norm=None)
+
+            self.assertEqual(test_result.shape, (5,))
+            self.assertEqual(test_result.dtype, np.float32)
+            np.testing.assert_array_equal(test_result, solution)
+
+            solution = np.array([1, 0.714286, 0.142857, 0.285714, 0], dtype=np.float32)
+            test_result = linear_residual(test_data_column, second_norm=zero_one)
+
+            self.assertEqual(test_result.shape, (5,))
+            self.assertEqual(test_result.dtype, np.float32)
+            np.testing.assert_array_almost_equal_nulp(test_result, solution, nulp=10)
+
+            solution = np.array([0, 0.99947, 0.999735, -0.124983, 0.285671], dtype=np.float32)
+            test_result = linear_residual(test_data_column, second_norm=percent_change)
+
+            self.assertEqual(test_result.shape, (5,))
+            self.assertEqual(test_result.dtype, np.float32)
+            np.testing.assert_array_almost_equal_nulp(test_result, solution, nulp=59)
+
+            solution = np.array([1.53393, 0.766965, -0.766965, -0.383482, -1.150447], dtype=np.float32)
+            test_result = linear_residual(test_data_column, second_norm=normal_dist)
+
+            self.assertEqual(test_result.shape, (5,))
+            self.assertEqual(test_result.dtype, np.float32)
+            np.testing.assert_array_almost_equal_nulp(test_result, solution, nulp=16)
+
+        def test_normalize_dataset(self):
+            pass
+
+        def tearDown(self):
+            pass
+
+
+    unittest.main(verbosity=2)
