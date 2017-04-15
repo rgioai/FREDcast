a9b8c222873eacd24c8c2875b9cb877b2b9c5a03
keithc <keithaf98@gmail.com>
2017-04-15 18:07:49 -0500

    Fixing a error that may be causing crash in norm_functions.py

--- norm_functions.py
+++ norm_functions.py
@@ -169,7 +169,8 @@ def normalize_dataset(dataset, norm_fn, second_norm=None):
 
     if norm_fn is not None:
         for i in range(0, copy.shape[1]):
-            copy[:, i] = norm_fn(copy[:, i], second_norm)
+            if not np.isnan(copy[:, i]).all():
+                copy[:, i] = norm_fn(copy[:, i], second_norm)
         return copy
     else:
         raise ValueError('Missing dataset value!')
