b72b80bd9263a8bf59df1ed5feeaaa2cc2ea33ed
gioGats <ryangiarusso@gmail.com>
2017-04-25 16:50:53 -0500

    Deploy code to buff, cobalt, fireball

--- deploy_code.sh
+++ deploy_code.sh
@@ -4,13 +4,13 @@
 # preferences of the user.  It's a hidden file, so whatever.  Except it throws off a lot of file operations
 # in this project.  Simply run this script to remove all the .DS_Store files in the data directory.
 
-find /centurion/FREDcast -name '.DS_Store' -type f -delete
+find ~/PycharmProjects/FREDcast -name '.DS_Store' -type f -delete
 
 # Copy relevant programs to deep learning servers
-scp -r ~/PycharmProjects/FREDcast/ rgio@cobalt.centurion.ai:/centurion/FREDcast/
+rsync -r ~/PycharmProjects/FREDcast rgio@cobalt.centurion.ai:/centurion/
 
-# rsync -r ~/PycharmProjects/FREDcast/ rgio@fireball.cs.uni.edu:/storage/xbrl_mapping/
+rsync -r ~/PycharmProjects/FREDcast rgio@fireball.cs.uni.edu:/centurion/
 
-scp -r ~/PycharmProjects/FREDcast/ rgio@buff.centurion.ai:/centurion/FREDcast/
+rsync -r ~/PycharmProjects/FREDcast rgio@buff.centurion.ai:/centurion/
 
-scp -r ~/PycharmProjects/FREDcast/ /centurion/FREDcast/
+# rsync -r ~/PycharmProjects/FREDcast/ /centurion/
