730d698cba48b7f64d88afa831126158f6482874
gioGats <ryangiarusso@gmail.com>
2017-04-25 22:08:04 -0500

    Function shells

---
+++ validation_functions.py
@@ -0,0 +1,16 @@
+
+
+def check_for_nan(hdf5_filepath):
+    pass
+    # TODO given a path to an hdf5, open it and check recursively for any nan values
+
+
+def check_dsets(hdf5_filepath):
+    pass
+    # TODO Based on hdf5_structure, check shapes and dtypes (preferably dynamically)
+
+
+if __name__ == '__main__':
+    pass
+    # TODO run check_for_nan on any hdf5 that should not have nan
+    # TODO run check_dsets on any hdf5 that will touch a classifier: probably split and rnn
